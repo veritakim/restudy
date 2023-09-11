c29e1543cd933385eb4fefc55e9c95c22152727e
veritakim <kjs3597@gmail.com>
2023-09-11 18:33:48 +0900

    graphql, typeorm

---
+++ study/day9/9-2-nestjs-with-typeorm/src/app.module.ts
@@ -0,0 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from './api/boards/entities/board.entity';
+import { BoardsModule } from './api/products/products.service';
+
+@Module({
+  imports: [
+    BoardsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [Board],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+})
+export class AppModule {}
