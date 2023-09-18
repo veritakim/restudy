2f903507d2d5848a7e3cbf4be256c50fdbb73c50
veritakim <kjs3597@gmail.com>
2023-09-18 22:12:54 +0900

    login jwt access-token

---
+++ study/day14/14-01-signup-bcrypt/src/apis/users/users.resolver.ts
@@ -0,0 +1,23 @@
+import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
+
+@Resolver()
+export class UsersResolver {
+  constructor(
+    private readonly usersService: UsersService, //
+  ) {}
+
+  @Mutation(() => User)
+  async createUser(
+    @Args('email') email: string,
+    @Args('password') password: string,
+    @Args('name') name: string,
+    @Args({ name: 'age', type: () => Int }) age: number,
+  ) {
+    const hashedPassword = await bcrypt.hash(password, 10.2);
+    console.log('해쉬드패스워드 : ', hashedPassword);
+    return this.usersService.create({ email, hashedPassword, name, age });
+  }
+}
---
+++ study/day14/14-01-signup-bcrypt/src/apis/users/users.service.ts
@@ -0,0 +1,29 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create({ email, hashedPassword: password, name, age }) {
+    const user = await this.usersRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (user) throw new ConflictException('이미 등록된 이메일입니다');
+
+    return this.usersRepository.save({
+      email, //
+      password,
+      name,
+      age,
+    });
+  }
+}
