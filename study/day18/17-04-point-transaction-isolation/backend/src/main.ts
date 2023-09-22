c0f308d2bf512566a61083224722d8909936a23d
veritakim <kjs3597@gmail.com>
2023-09-22 14:09:35 +0900

    make a directory

---
+++ study/day18/17-04-point-transaction-isolation/backend/src/main.ts
@@ -0,0 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // class-validation 검증기능
+  app.useGlobalPipes(new ValidationPipe());
+  // http-exception.filter 등록하기
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(3000);
+}
+bootstrap();
