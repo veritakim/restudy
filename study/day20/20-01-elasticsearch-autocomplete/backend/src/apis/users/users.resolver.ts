0b270fd5d91e3470b058825e4f25fce25034f314
veritakim <kjs3597@gmail.com>
2023-09-27 22:33:13 +0900

    elasticsearch autocomplete

---
+++ study/day20/20-01-elasticsearch-autocomplete/backend/src/apis/users/users.resolver.ts
@@ -0,0 +1,37 @@
+import { Args, Context, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GqlAuthAccessGuard } from '../../commons/auth/gql-auth.guard';
+
+@Resolver()
+export class UsersResolver {
+  constructor(
+    private readonly usersService: UsersService, //
+  ) {}
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Query(() => String)
+  fetchUser(
+    @Context() context: any, //
+  ) {
+    console.log(context.req.user);
+    // 유저정보 꺼내오기
+    console.log('fetchUser 실행완료');
+
+    return 'fetchUser 실행';
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Args('email') email: string,
+    @Args('password') password: string,
+    @Args('name') name: string,
+    @Args({ name: 'age', type: () => Int }) age: number,
+  ) {
+    const hashedPassword = await bcrypt.hash(password, 10.2);
+    return this.usersService.create({ email, hashedPassword, name, age });
+  }
+}
